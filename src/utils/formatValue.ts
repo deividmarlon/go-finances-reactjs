ffec87dfcfb3e481528f17a2279de46f8b097a27
Deivid Márlon <deividmarlon@gmail.com>
2020-09-14 15:22:38 -0300

    get Balance | get Transcations | header Nav | import csv -> done!

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,13 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  let formattedValue = Intl.NumberFormat('br-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+  formattedValue = formattedValue.includes(',')
+    ? formattedValue
+    : `${formattedValue},00`;
+  return formattedValue;
+};
+// TODO
 
 export default formatValue;
